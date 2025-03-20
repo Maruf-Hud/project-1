e4134dda5bb8b12b697e25fb09c2a0f9952b1cfd
Md Maruf (U2474162) <U2474162@unimail.hud.ac.uk>
2025-03-20 15:50:32 +0000

    trying to load image properly

--- game.js
+++ game.js
@@ -2,12 +2,27 @@ const body = document.querySelector("body");
 const canvas = document.querySelector("canvas#game");
 const Menu = document.querySelector(".main_menu");
 var ctx = canvas.getContext("2d");
+const mainMap = new Image();
+// const mainMap = document.querySelector("#main_map");
 
 const newGame = () =>{
+    document.querySelector("#plane").classList.add("flynow");
     setTimeout(() => {
-        Menu.classList.add("d-none")
-    }, 2500);
-    document.querySelector("img").classList.add("flynow");
+        loadGame(); 
+    }, 150);
 }
 
-// some changes
+newGame();
+
+const loadGame = () => {
+    Menu.classList.add("d-none");
+    canvas.classList.remove("d-none");
+    let cX = window.innerWidth;
+    let cY = window.innerHeight;
+    console.log(cX, cY)
+    // Make sure the image is loaded before drawing
+    mainMap.onload = () => {
+        ctx.drawImage(mainMap, 0, 0, 300, 150);
+    }
+    mainMap.src="/assets/images/basicMap.png"
+};
